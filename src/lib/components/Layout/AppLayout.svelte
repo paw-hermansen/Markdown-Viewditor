e2c8b963d64d438fb2fc1d201929da340e2739cd
Paw Hermansen <email@pawhermansen.dk>
2026-09-16 23:32:07 +0200

    Show full file path as tooltip on filename hover in toolbar

--- src/lib/components/Layout/AppLayout.svelte
+++ src/lib/components/Layout/AppLayout.svelte
@@ -173,7 +173,7 @@
       </button>
       {#if fileName}
         <span class="separator"></span>
-        <span class="file-name">{fileName}{#if isModified} *{/if}{#if fileState.isReadOnly} <span class="readonly-indicator" title="Read-only">&#x1F512;</span>{/if}{#if fileState.externallyModified} <span class="external-change-warning" title="Externally modified">&#x26A0;</span>{/if}</span>
+        <span class="file-name" title={fileState.currentFile ?? ''}>{fileName}{#if isModified} *{/if}{#if fileState.isReadOnly} <span class="readonly-indicator" title="Read-only">&#x1F512;</span>{/if}{#if fileState.externallyModified} <span class="external-change-warning" title="Externally modified">&#x26A0;</span>{/if}</span>
       {/if}
     </div>
     <div class="toolbar-center">
